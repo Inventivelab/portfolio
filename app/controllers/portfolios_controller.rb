5cbd63ab6620a2ed98bc8ec802448c0c4783a488
Said Maadan <me@saidmaadan.com>
2017-09-12 21:35:52 -0500

    Created portfolios new and created actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,4 +3,22 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to portfolios_path, notice: "Your portfolio is now published"
+    else
+      render :new
+    end
+  end
+
+  private
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body, :display_image, :thumb_image)
+  end
 end
