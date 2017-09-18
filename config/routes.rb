15208dfcda83aa7855995f1485030146b08a43e4
Said Maadan <me@saidmaadan.com>
2017-09-18 08:43:09 -0500

    modified routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -13,3 +13,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+# get 'myabout/login/page', to: 'pages#about', as: 'about'
+# namespace :admin do
+#   get 'dashboard/main'
+#   get 'dashboard/user'
+#   get 'dashboard/login'
+# end
+# get 'posts/*missing', to: 'posts#missing'
