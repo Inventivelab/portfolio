eea70e49b3cc22a71aa88f61d40be87e8baf1d07
Said Maadan <me@saidmaadan.com>
2017-09-22 14:40:22 -0500

    Integrated content tag \helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
        (link_to "Register", new_user_registration_path)
     end
   end
+
+  def source_helper(layout_name)
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
+  end
+
+  def sample_helper
+    content_tag(:div, "My content", class: "My class")
+  end
 end
