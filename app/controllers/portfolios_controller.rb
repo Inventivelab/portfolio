c0dfe606c62c2beab3e5542da7a1529d7a085d72
Said Maadan <me@saidmaadan.com>
2017-09-29 11:53:53 -0500

    Implemented persistent drag and drop position

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -10,6 +10,13 @@ class PortfoliosController < ApplicationController
     # @portfolios = Portfolio.where(subtitle: 'Javasscript')
   end
 
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+    render nothing: true
+  end
+
   def rubyonrails
     @portfolios = Portfolio.ruby_on_rails_portfolio_items
   end
