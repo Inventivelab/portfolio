dd968e3dcbd8fdf707007d443df68ef44dd5aa19
Said Maadan <me@saidmaadan.com>
2017-10-08 17:54:04 -0500

    Setup action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://inventive-portfolio.herokuapp.com', 'http://inventive-portfolio.herokuapp.com']
+  config.action_cable.url = "wss://inventive-portfolio.herokuapp.com/cable"
 end
